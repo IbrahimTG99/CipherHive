89f8f92b720f82a326e270975ef66f054f165e60
IbrahimTariqDev <ibrahim.tariq@devsinc.com>
2023-05-03 20:56:42 +0500

    Added Data Store dependency. implemented storing information in datastore using custom serializer class. Used keystore to generate key and encrypt/decrypt data from data store.

--- app/src/main/java/com/devsinc/cipherhive/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/devsinc/cipherhive/presentation/profile/ProfileScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -17,29 +19,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.datastore.core.DataStore
 import coil.compose.AsyncImage
 import com.devsinc.cipherhive.CryptoManager
+import com.devsinc.cipherhive.model.UserDatum
 import com.devsinc.cipherhive.presentation.sign_in.UserData
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
 
 @Composable
 fun ProfileScreen(
-    userData: UserData?, onSignOut: () -> Unit, cryptoManager: CryptoManager, filesDir: File
+    userData: UserData?,
+    onSignOut: () -> Unit,
+    cryptoManager: CryptoManager,
+    filesDir: File,
+    dataStore: DataStore<UserDatum>
 ) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.size(16.dp))
         if (userData?.profilePictureUrl != null) {
             AsyncImage(
                 model = userData.profilePictureUrl,
@@ -62,48 +73,62 @@ fun ProfileScreen(
         Button(onClick = onSignOut) {
             Text(text = "Sign Out")
         }
-    }
-    // demo for testing encryption
-    var messageToEncrypt by remember { mutableStateOf("") }
 
-    var messageToDecrypt by remember { mutableStateOf("") }
+        Spacer(modifier = Modifier.size(16.dp))
+        // demo for testing encryption
+        var messageToEncrypt by remember { mutableStateOf("") }
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        TextField(value = messageToEncrypt,
-            onValueChange = { messageToEncrypt = it },
-            modifier = Modifier.fillMaxWidth(),
-            placeholder = { Text(text = "Enter message to encrypt") })
-        Spacer(modifier = Modifier.size(8.dp))
-        Row {
-            Button(onClick = {
-                val bytes = messageToEncrypt.encodeToByteArray()
-                val file = File(filesDir, "test.txt")
-                if (!file.exists()) {
-                    file.createNewFile()
+        var messageToDecrypt by remember { mutableStateOf("") }
+
+        var username by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var readData by remember { mutableStateOf(UserDatum()) }
+
+        val scope = rememberCoroutineScope()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp)
+        ) {
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                placeholder = { Text("Username") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                placeholder = { Text("Password") },
+                modifier = Modifier.fillMaxWidth(),
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Row {
+                Button(onClick = {
+                    scope.launch {
+                        dataStore.updateData {
+                            UserDatum(
+                                username = username, password = password
+                            )
+                        }
+                    }
+                }) {
+                    Text(text = "Save")
                 }
-                val fos = FileOutputStream(file)
-                messageToDecrypt = cryptoManager.encrypt(
-                    data = bytes,
-                    outputStream = fos
-                ).decodeToString()
-
-            }) {
-                Text(text = "Encrypt")
+                Spacer(modifier = Modifier.width(8.dp))
+                Button(onClick = {
+                    scope.launch {
+                        readData = dataStore.data.first()
+                    }
+                }) {
+                    Text(text = "Read")
+                }
             }
-            Spacer(modifier = Modifier.width(16.dp))
-            Button(onClick = {
-                val file = File(filesDir, "test.txt")
-                messageToEncrypt = cryptoManager.decrypt(
-                    inputStream = FileInputStream(file)
-                ).decodeToString()
-            }) {
-                Text(text = "Decrypt")
-            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Read data: $readData")
         }
-        Text(text = messageToDecrypt)
     }
 }
